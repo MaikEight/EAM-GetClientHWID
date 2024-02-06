f6d3d148190b0bf4c34d5cbf79bc50cc4f256a42
MaikEight <mail@maik8.de>
2024-02-06 02:48:45 +0100

    🎨 Refactor GetHWID class to improve readability and remove unnecessary code

--- Assets/GetHWID.cs
+++ Assets/GetHWID.cs
@@ -8,24 +8,19 @@ public class GetHWID : MonoBehaviour
         {
             string[] args = System.Environment.GetCommandLineArgs();
             Debug.Log(System.Environment.CommandLine);
-            //"EAM GetClientHWID.exe" -nographics -batchmode {C:\Users\Maik8\Documents\Unity\EAM GetClientHWID\Build}
             Debug.Log($"{System.Environment.NewLine}HWID: {SystemInfo.deviceUniqueIdentifier}{System.Environment.NewLine}");
 
             string dir = System.IO.Path.GetFullPath(".");
-            //string t = "\"EAM GetClientHWID.exe\" - nographics - batchmode {C:\\Users\\Maik8\\Documents\\Unity\\EAM GetClientHWID\\Build\\asd}";
-            //if (!string.IsNullOrWhiteSpace(t) && t.Contains("{") && t.Contains("}"))
             if (!string.IsNullOrWhiteSpace(System.Environment.CommandLine) && System.Environment.CommandLine.Contains("{") && System.Environment.CommandLine.Contains("}"))
-            {
-                //dir = t.Substring(t.IndexOf("{") + 1, (t.IndexOf("}") - 1) - t.IndexOf("{"));
+            {                
                 dir = System.Environment.CommandLine.Substring(System.Environment.CommandLine.IndexOf("{") + 1, (System.Environment.CommandLine.IndexOf("}") - 1)- System.Environment.CommandLine.IndexOf("{"));
             }
-            Debug.Log($"|{dir}|");
+            Debug.Log(dir);
 
             if (!System.IO.Directory.Exists(dir))
                 System.IO.Directory.CreateDirectory(dir);
 
-            System.IO.File.WriteAllText(System.IO.Path.Combine(dir, "EAM.HWID"), SystemInfo.deviceUniqueIdentifier);
-            
+            System.IO.File.WriteAllText(System.IO.Path.Combine(dir, "EAM.HWID"), SystemInfo.deviceUniqueIdentifier);            
         }
         catch (System.Exception e)
         {
